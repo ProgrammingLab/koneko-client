12df07374a3f1a00f43220bc5c8655e13011d7c2
fukusan <ikishikawa64@gmail.com>
2018-03-29 22:38:16 +0900

    loginと書いたところをLoginに変更

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '@/components/login';
+import Login from '@/components/Login';
 
 Vue.use(Router);
 
@@ -8,7 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'login',
+      name: 'Login',
       component: Login,
     },
   ],
