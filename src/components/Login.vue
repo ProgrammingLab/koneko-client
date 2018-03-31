abcfe2b3d33b6fc1686d50c6be0fa54b89d6cacb
fukusan <ikishikawa64@gmail.com>
2018-04-01 03:48:01 +0900

    タグが長くなっていたので改行を入れた

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,16 +9,32 @@
             <form>
               <div class="field">
                 <div class="control">
-                  <input class="input is-large" type="email" placeholder="Your Email" autofocus="">
+                  <input
+                    class="input is-large"
+                    type="email"
+                    placeholder="Your Email"
+                    autofocus=""
+                    v-model="email"
+                  >
                 </div>
               </div>
 
               <div class="field">
                 <div class="control">
-                  <input class="input is-large" type="password" placeholder="Your Password">
+                  <input
+                    class="input is-large"
+                    type="password"
+                    placeholder="Your Password"
+                    v-model="password"
+                  >
                 </div>
               </div>
-              <button class="button is-block is-info is-large is-fullwidth">Login</button>
+              <button
+                class="button is-block is-info is-large is-fullwidth"
+                @click="onLogin"
+              >
+                Login
+              </button>
             </form>
           </div>
           <p class="has-text-grey">
