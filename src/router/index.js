cb3130b2e57d77c54b92e9d154b31893a98b71d9
fukusan <ikishikawa64@gmail.com>
2018-04-02 00:47:27 +0900

    ハリボテtopページを追加

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/components/Login';
+import Top from '@/components/Top';
 
 Vue.use(Router);
 
 export default new Router({
+// TODO:権限の確認とログインページの読み飛ばしやって
+  mode: 'history',
   routes: [
     {
       path: '/',
+      name: 'Top',
+      component: Top,
+    },
+    {
+      path: '/login',
       name: 'Login',
       component: Login,
     },
