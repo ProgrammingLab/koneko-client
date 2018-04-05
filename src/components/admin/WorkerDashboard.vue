7f2de5e00e88b90fd352851be675baefdd06eb6c
gedorinku <gedorinku@yahoo.co.jp>
2018-04-05 18:19:00 +0900

    とりあえず取得ができた🤗

--- src/components/admin/WorkerDashboard.vue
+++ src/components/admin/WorkerDashboard.vue
@@ -6,10 +6,8 @@ import { mapActions, mapState } from 'vuex';
 
 export default {
   name: 'WorkerDashboard',
-  mounted() {
-    // eslint-disable-next-line no-console
-    console.error('hoge');
-    this.getWorkers(this.sessionID);
+  created() {
+    this.getWorkers({ sessionID: this.sessionID });
   },
   computed: {
     ...mapState('auth', ['sessionID']),
