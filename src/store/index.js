d9f790a3c7c8b5b942ec904f773bc021ec055a50
fukusan <ikishikawa64@gmail.com>
2018-04-14 23:40:29 +0900

    sessionIDをsessionStrageに保存することで永続化した

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import Vuex from 'vuex';
 import Vue from 'vue';
+import createPersistedState from 'vuex-persistedstate';
 import koneko from './modules/koneko';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
+  plugins: [
+    createPersistedState({
+      paths: ['koneko.sessionID'],
+      storage: window.sessionStorage,
+    }),
+  ],
   modules: {
     koneko,
   },
