977ca198833730ababce73f50ecae1be1db5735a
gedorinku <gedorinku@yahoo.co.jp>
2018-04-18 08:36:10 +0900

    見た目だけ。

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Login from '@/components/Login';
 import Admin from '@/components/admin/Admin';
 import Top from '@/components/Top';
+import NewContest from '@/components/NewContest';
 import NotFound from '@/components/NotFound';
 import store from '@/store';
 
@@ -26,6 +27,12 @@ const router = new Router({
       name: 'Admin',
       component: Admin,
       meta: { requiresAuth: true, admin: true },
+    },
+    {
+      path: '/contests/new',
+      name: 'NewContest',
+      component: NewContest,
+      meta: { requiresAuth: true, admin: false },
     },
     {
       path: '*',
