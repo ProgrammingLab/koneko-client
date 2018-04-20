eac4e5fe6b900d002e9c5a8ce21bb8d366af3e90
fukusan <ikishikawa64@gmail.com>
2018-04-21 00:15:28 +0900

    とりあえず見た目だけ作った

--- src/components/contest/Contest.vue
+++ src/components/contest/Contest.vue
@@ -1,10 +1,163 @@
+
 <template>
-  <h1>Contest Page だよ</h1>
+  <div>
+    <div class="container margin-bottom">
+      <section class="hero is-small is-light">
+        <div class="hero-body">
+          <nav class="navbar">
+            <div class="navbar-brand">
+              <div>
+                <h1 class="title">未実装ガバガバコンテスト</h1>
+                <h2 class="subtitle is-6">完成予定: 2018/4/18~2018/5/18</h2>
+              </div>
+            </div>
+            <div id="navbarMenu" class="navbar-menu">
+              <div class="navbar-end">
+                <span class="navbar-item">
+                  <button class="button is-outlined" href="#">
+                    提出一覧
+                  </button>
+                  <button class="button is-outlined" href="#">
+                    順位
+                  </button>
+                </span>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </section>
+      <article class="message is-light">
+        <div class="message-header">
+          <p>詳細</p>
+        </div>
+        <div class="message-body" v-if="true">
+          <p>
+            福さんの実装の遅さのために進捗が全く出ていないこと<br>
+            昨日、commit0回,push0回であったことをここに謝罪します。
+          </p>
+        </div>
+    </article>
+    </div>
+    <div class="container">
+      <div class="columns">
+        <div class="column is-1 tab">
+          <aside>
+            <ul class="menu-list">
+              <li>
+                <a href="#a" class="tab-button active-tab-button">
+                  A
+                  <tag :status="0"/>
+                </a>
+              </li>
+              <li>
+                <a href="#b" class="tab-button">
+                  B
+                  <tag :status="1"/>
+                </a>
+              </li>
+              <li>
+                <a href="#c" class="tab-button">
+                  C
+                  <tag :status="2"/>
+                </a>
+              </li>
+              <li>
+                <a href="#d" class="tab-button">
+                  D
+                  <tag :status="3"/>
+                </a>
+              </li>
+              <li>
+                <a href="#e" class="tab-button">
+                  E
+                  <tag :status="4"/>
+                </a>
+              </li>
+              <li>
+                <a href="#e" class="tab-button">
+                  F
+                  <tag :status="5"/>
+                </a>
+              </li>
+              <li>
+                <a href="#e" class="tab-button">
+                  G
+                  <tag :status="6"/>
+                </a>
+              </li>
+              <li>
+                <a href="#e" class="tab-button">
+                  H
+                  <tag :status="7"/>
+                </a>
+              </li>
+              <li>
+                <a href="#e" class="tab-button">
+                  I
+                  <tag :status="8"/>
+                </a>
+              </li>
+              <li>
+                <a href="#e" class="tab-button">
+                  J
+                  <tag :status="9"/>
+                </a>
+              </li>
+            </ul>
+          </aside>
+        </div>
+        <div class="column">
+          <!-- ここは問題描画用のコンポーネントを使う -->
+          <h1 class="title underline"><a href="#">#1</a>進捗どうですか</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Tag from './Tag';
 
 export default {
   name: 'Contest',
+  components: {
+    Tag,
+  },
 };
 </script>
+
+<style scoped>
+  #contest-title{
+    margin-bottom: 10px;
+  }
+  .tab {
+    padding-right: 0;
+    border-right: 5px solid hsl(204, 86%, 53%);
+  }
+  .tab-button{
+    border: 1px solid silver;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    background-color: white;
+  }
+  .ac{
+    background-color: hsl(141, 71%, 48%);
+  }
+  .ac:hover{
+    background-color: hsl(141, 71%, 48%);
+  }
+  .active-tab-button{
+    border-right-color: hsl(204, 86%, 53%);
+    background-color: hsl(204, 86%, 53%);
+  }
+  .active-tab-button:hover{
+    background-color: hsl(204, 86%, 53%);
+  }
+  .margin-bottom{
+    margin-bottom: 20px;
+  }
+  .underline{
+    padding-bottom:10px;
+    border-bottom: 1px solid gray;
+  }
+</style>
---
+++ src/components/contest/Tag.vue
@@ -0,0 +1,37 @@
+<template>
+  <span :class="getStatusStyle" v-if="0 <= status && status < 11">
+    <span class="icon" v-if="status <= 1">
+      <i class="fas fa-spinner fa-spin"></i>
+    </span>
+    <template v-else>
+      {{ getStatusCode }}
+    </template>
+  </span>
+</template>
+
+
+<script>
+export default {
+  name: 'Tag',
+  props: { status: Number },
+  computed: {
+    getStatusCode() {
+      const code = [
+        'WJ', 'WJ', 'AC', 'PE', 'WA', 'TLE',
+        'MLE', 'RE', 'CE', 'OLE', 'UE',
+      ];
+      return code[this.status];
+    },
+    getStatusStyle() {
+      const style = [
+        '', '', 'is-success', 'is-warning', 'is-warning', 'is-warning',
+        'is-warning', 'is-danger', 'is-danger', 'is-danger', 'is-danger',
+      ];
+      return `${style[this.status]} tag is-rounded is-pulled-right`;
+    },
+  },
+};
+</script>
+
+<style lang="css">
+</style>
