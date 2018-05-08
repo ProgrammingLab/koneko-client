f35d187878de4f7a266950b6c40ac1b915e681cf
fukusan <ikishikawa64@gmail.com>
2018-05-08 18:04:25 +0900

    日付3桁もいらんやろ2桁にした

--- src/components/contest/Contest.vue
+++ src/components/contest/Contest.vue
@@ -133,11 +133,11 @@ export default {
       const serverTime = this.now.add(this.timeDiff);
       const diff = moment(this.startAt).diff(serverTime);
       if (diff < 0) return '';
-      const DDD = `000${Math.floor(diff / 1000 / 60 / 60 / 24)}`.slice(-3);
+      const DD = `00${Math.floor(diff / 1000 / 60 / 60 / 24)}`.slice(-3);
       const HH = `00${Math.floor(diff / 1000 / 60 / 60) % 24}`.slice(2);
       const mm = `00${Math.floor(diff / 1000 / 60) % 60}`.slice(-2);
       const ss = `00${Math.floor(diff / 1000) % 60}`.slice(-2);
-      return `${DDD}日${HH}時間${mm}分${ss}秒`;
+      return `${DD}日${HH}時間${mm}分${ss}秒`;
     },
   },
   created() {
