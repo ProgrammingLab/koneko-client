aacaee4ad8c683d69a6370efd1273aa39886db00
fukusan <ikishikawa64@gmail.com>
2018-05-12 01:23:06 +0900

    モーダルに横幅を大きくするオプションを追加

--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="modal" :class="{ 'is-active': isActive }">
     <div class="modal-background" @click="$emit('close')"></div>
-    <div class="modal-card">
+    <div class="modal-card" :class="{wide: isWide}">
       <header class="modal-card-head">
         <p class="modal-card-title">{{title}}</p>
         <button class="delete" aria-label="close" @click="$emit('close')"></button>
@@ -20,6 +20,12 @@ export default {
   props: {
     title: String,
     isActive: Boolean,
+    isWide: Boolean,
   },
 };
 </script>
+<style scoped>
+  .wide{
+    width: 90%;
+  }
+</style>
