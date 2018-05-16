76a3fc8f9b83bc0a4bee83f191dbf8550951eb00
Fukusan64 <ikishikawa64@gmail.com>
2018-05-16 11:53:57 +0900

    入力をSubmitModalのdataと同期させた

--- src/components/problem/SubmitModal.vue
+++ src/components/problem/SubmitModal.vue
@@ -1,6 +1,10 @@
 <template>
   <Modal title="提出" :isActive="isActive" @close="$emit('close')">
-    <SourceCodeInput value="{languageID: 1, sourceCode: ''}" :title="problem.title"/>
+    <SourceCodeInput
+      :value="value"
+      :title="problem.title"
+      @input="updateValue"
+    />
     <button class="button is-link" @click="$emit('submit')">
       提出
     </button>
@@ -11,10 +15,23 @@ import Modal from '@/components/common/Modal';
 import SourceCodeInput from '@/components/common/SourceCodeInput';
 
 export default {
+  data() {
+    return {
+      value: {
+        languageID: 1,
+        sourceCode: '',
+      },
+    };
+  },
   props: [
     'isActive',
     'problem',
   ],
+  methods: {
+    updateValue(newValue) {
+      this.value = newValue;
+    },
+  },
   components: {
     Modal,
     SourceCodeInput,
