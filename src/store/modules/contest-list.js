1138044a02f6a110f901f73250a2a6cd40758527
gedorinku <gedorinku@yahoo.co.jp>
2018-05-18 16:13:23 +0900

    トップページを作った。

--- src/store/modules/contest-list.js
+++ src/store/modules/contest-list.js
@@ -6,6 +6,36 @@ export default {
     contests: null,
     error: null,
   },
+  getters: {
+    activeContests(state, getters, rootState, rootGetters) {
+      const serverTime = rootGetters['koneko/timeDiff/serverTime'];
+      const contests = state.contests;
+      if (contests) {
+        return contests.filter(c =>
+          serverTime.isSameOrAfter(c.startAt) && serverTime.isBefore(c.endAt),
+        );
+      }
+      return null;
+    },
+    upcomingContests(state, getters, rootState, rootGetters) {
+      const serverTime = rootGetters['koneko/timeDiff/serverTime'];
+      const contests = state.contests;
+      if (contests) {
+        return contests.filter(c => serverTime.isBefore(c.startAt));
+      }
+      return null;
+    },
+    recentContests(state, getters, rootState, rootGetters) {
+      const serverTime = rootGetters['koneko/timeDiff/serverTime'];
+      const contests = state.contests;
+      if (contests) {
+        return contests.filter(c =>
+          serverTime.isSameOrAfter(c.endAt)).slice(0, Math.min(5, contests.length),
+        );
+      }
+      return null;
+    },
+  },
   mutations: {
     setContests(state, contests) {
       state.contests = contests;
