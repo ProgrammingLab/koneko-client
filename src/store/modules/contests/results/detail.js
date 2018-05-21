a2d185851a8b0fc162ae3554bc38f48e8d4cbd9f
Fukusan64 <ikishikawa64@gmail.com>
2018-05-21 19:10:36 +0900

    提出詳細のテストケースの上に更新ボタンをつけた

--- src/store/modules/contests/results/detail.js
+++ src/store/modules/contests/results/detail.js
@@ -3,19 +3,23 @@ import api from '@/api';
 export default {
   namespaced: true,
   state: {
+    submissionID: null,
     detail: {},
   },
   mutations: {
+    setSubmissionID(state, submissionID) {
+      state.submissionID = submissionID;
+    },
     setDetail(state, detail) {
       state.detail = detail;
     },
   },
   actions: {
-    async getDetail({ commit, rootState }, id) {
+    async getDetail({ commit, rootState, state }) {
       try {
         const res = await api.getResultDetail(
           rootState.koneko.sessionID,
-          id,
+          state.submissionID,
         );
         commit('setDetail', res.data);
       } catch (e) {
