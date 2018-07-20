8e461947b26731ccef878b13c99bd1f2fe3e661c
Fukusan64 <ikishikawa64@gmail.com>
2018-07-20 21:20:49 +0900

    確認モーダルにコンテスト時間を出すようにした

--- src/components/contest/EnterConfirmationModal.vue
+++ src/components/contest/EnterConfirmationModal.vue
@@ -4,7 +4,10 @@
     title="参加確認"
     @close="close"
   >
-    <p>参加するとすぐにコンテストが始まります。</p>
+    <p>
+      参加するとすぐにコンテストが始まります。<br/>
+      コンテスト時間は、参加から{{Math.floor(duration / 60)}}分です
+    </p>
     <div slot="footer">
       <button class="button is-success" @click="myEnter">参加する</button>
       <button class="button" @click="close">キャンセル</button>
@@ -22,6 +25,7 @@ export default {
   ],
   computed: {
     ...mapState('koneko/contests', [
+      'duration',
       'id',
     ]),
   },
